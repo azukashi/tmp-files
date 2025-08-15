db7317445ae6c7503d6213ae769d4a811c27e909
Azka <falcxxdev@pm.me>
2025-08-15 08:53:52 +0000

    refactor(title): improve web title component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="font-vt_323">
-        <h1 class="text-7xl">/tmp/</h1>
-        <p class="text-lg line-clamp-2">
-            Temporary file upload.
-            <br />
-            Files automatically deleted after 1 hour.
-        </p>
-        <p>Maximum file size is 100MB.</p>
+    <div id="title" class="font-vt_323 flex flex-col gap-3 align-middle items-center justify-center mb-6">
+        <div class="flex gap-3">
+            <img src="/icon.png" alt="Web icon" />
+            <h1 class="text-7xl">/tmp/fup</h1>
+        </div>
+        <div class="text-left font-fira_code">
+            <p class="text-sm line-clamp-2">
+                Temporary file upload.
+                <br />
+                Files automatically deleted after 1 hour.
+            </p>
+            <p class="text-sm line-clamp-2">Maximum file size is 100MB.</p>
+        </div>
     </div>
 </template>
 
